716ef992f7a9d12e943284014498004044d2b09b
Isabelly Lacerda <santosisabelly771@gmail.com>
2025-06-25 09:49:32 -0300

    scritps

---
+++ scripts/calculadora.js
@@ -0,0 +1,25 @@
+function somar() {
+  let num1 = document.getElementById("num1").value;
+  let num2 = document.getElementById("num2").value;
+  let soma = parseFloat(num1) + parseFloat(num2);
+  document.getElementById("resultado").innerHTML = "Resultado: " + soma;
+}
+
+function subtrair() {
+  let num1 = document.getElementById("num1").value;
+  let num2 = document.getElementById("num2").value;
+  let resultado = parseFloat(num1) - parseFloat(num2);
+  document.getElementById("resultado").innerHTML = "Resultado: " + resultado;
+}
+function dividir() {
+  let num1 = document.getElementById("num1").value;
+  let num2 = document.getElementById("num2").value;
+  let resultado = parseFloat(num1) / parseFloat(num2);
+  document.getElementById("resultado").innerHTML = "Resultado: " + resultado;
+}
+function multiplicar() {
+  let num1 = document.getElementById("num1").value;
+  let num2 = document.getElementById("num2").value;
+  let resultado = parseFloat(num1) * parseFloat(num2);
+  document.getElementById("resultado").innerHTML = "Resultado: " + resultado;
+}
